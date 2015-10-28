ae88b66d30ca176ae2d2bb3d42dc43948becdeb0
wangyong <whuwangyong@gmail.com>
2015-10-28 18:34:28 +0800

    cancel apt-get install virtualbox

--- cloudlab_env_ovs.sh
+++ cloudlab_env_ovs.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 echo Yong:1|sudo chpasswd
-echo 'deb http://download.virtualbox.org/virtualbox/debian trusty contrib' | sudo tee -a /etc/apt/sources.list
 sudo apt-get update
-sleep 60 # 1 min
 sudo apt-get install -y vim openvswitch-switch virtualbox-5.0 dkms axel
-sleep 120 # 2 min
 sudo apt-get -f install
-sleep 60 # 1 min
+axel http://download.virtualbox.org/virtualbox/5.0.8/virtualbox-5.0_5.0.8-103449~Ubuntu~precise_amd64.deb
+sudo dpkg -i virtualbox-5.0_5.0.8-103449~Ubuntu~precise_amd64.deb
 axel http://download.virtualbox.org/virtualbox/5.0.8/Oracle_VM_VirtualBox_Extension_Pack-5.0.8-103449.vbox-extpack
-sleep 60 # 1 min
 sudo vboxmanage extpack install Oracle_VM_VirtualBox_Extension_Pack-5.0.8-103449.vbox-extpack
 
