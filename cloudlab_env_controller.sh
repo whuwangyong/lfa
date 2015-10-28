c0a375150df5515bcc78b05219dff7e371ce3e46
wangyong <whuwangyong@gmail.com>
2015-10-28 19:04:22 +0800

    m

--- cloudlab_env_controller.sh
+++ cloudlab_env_controller.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 echo Yong:1|sudo chpasswd
 sudo apt-get update
-sleep 60 # 1min
 sudo apt-get install -y vim openjdk-7-jdk ant axel
-sleep 120 #2 min
 git clone https://github.com/floodlight/floodlight.git
-sleep 60
 cd floodlight
 ant
-sleep 120 #2 min
 sudo java -jar target/floodlight.jar
