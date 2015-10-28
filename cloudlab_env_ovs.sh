c261d0e6f0b3cb412f4de662e90bcc8edc597159
wangyong <whuwangyong@gmail.com>
2015-10-28 18:58:03 +0800

    m

--- cloudlab_env_ovs.sh
+++ cloudlab_env_ovs.sh
@@ -4,7 +4,7 @@ sudo apt-get update
 sudo apt-get install -y vim openvswitch-switch dkms axel
 axel http://download.virtualbox.org/virtualbox/5.0.8/virtualbox-5.0_5.0.8-103449~Ubuntu~precise_amd64.deb
 sudo dpkg -i virtualbox-5.0_5.0.8-103449~Ubuntu~precise_amd64.deb
-sudo apt-get -f install
+sudo apt-get -f install -y
 axel http://download.virtualbox.org/virtualbox/5.0.8/Oracle_VM_VirtualBox_Extension_Pack-5.0.8-103449.vbox-extpack
 sudo vboxmanage extpack install Oracle_VM_VirtualBox_Extension_Pack-5.0.8-103449.vbox-extpack
 
