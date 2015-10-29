4b1a5a053d0f53947c6dd013299897ff8b4cf83b
wangyong <whuwangyong@gmail.com>
2015-10-29 10:12:43 +0800

    add set dpid

--- cloudlab_create_ovs.sh
+++ cloudlab_create_ovs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo ovs-vsctl add-br br0
 sudo ovs-vsctl set-controller br0 tcp:155.98.39.124:6653
-
+sudo ovs-vsctl set bridge br0 other-config:datapath-id=000000000000000$1;
 
 function add_port()
 {
