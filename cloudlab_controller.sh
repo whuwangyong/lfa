d442bcb0bcfdc76b5eb3ee9ef9b38cb08c08bc33
wangyong <whuwangyong@gmail.com>
2015-10-29 16:04:03 +0800

    add quiet args

--- cloudlab_controller.sh
+++ cloudlab_controller.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo Yong:1|sudo chpasswd
-sudo apt-get update
+sudo apt-get update -q=2
 sudo apt-get install -y vim openjdk-7-jdk ant axel
 git clone https://github.com/floodlight/floodlight.git
 cd floodlight
