97f334ecad26e9d1a42073897711a5de0ded34b6
wangyong <whuwangyong@gmail.com>
2015-10-29 15:46:18 +0800

    m

---
+++ cloudlab_boundary_ovs.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+echo Yong:1|sudo chpasswd
+if [ -z "$1" ]
+then
+	echo "need an argument as a part of ovs dpid\n"
+	echo "such as 01,09,0a,11"
+	exit
+fi
+
+sudo apt-get update
+sudo apt-get install -y vim openvswitch-switch dkms axel
+axel http://download.virtualbox.org/virtualbox/5.0.8/virtualbox-5.0_5.0.8-103449~Ubuntu~precise_amd64.deb
+sudo dpkg -i virtualbox-5.0_5.0.8-103449~Ubuntu~precise_amd64.deb
+sudo apt-get -f install -y
+axel http://download.virtualbox.org/virtualbox/5.0.8/Oracle_VM_VirtualBox_Extension_Pack-5.0.8-103449.vbox-extpack
+sudo vboxmanage extpack install Oracle_VM_VirtualBox_Extension_Pack-5.0.8-103449.vbox-extpack
+echo "************ vim openvswitch-switch virtualbox is installed *****************"
+
+sudo ovs-vsctl add-br br0 
+sudo ovs-vsctl set bridge br0 other-config:datapath-id=00aa00bb00cc00$1;
+sudo ovs-vsctl set-controller br0 tcp:155.98.39.124:6653
+
+function add_port()
+{
+	ovs=${1};
+	node=${2};
+
+	echo $ovs --tap port-- $node;
+	port=vnet-${ovs}-${node};
+
+	sudo ip tuntap add mode tap $port;
+	sudo ip link set $port up;
+	sudo ovs-vsctl add-port $ovs $port;
+}
+
+add_port br0 h1
+add_port br0 h2
+echo "************ ovs and port is created *****************"
+
+# create vm and start them
+# requirement:
+# 1. openvswitch br0 and vnet-port
+# 2. VirtualBox and extpack installed
+
+vboxmanage import host.ova
+vboxmanage modifyvm host --name h1
+vboxmanage import host.ova
+vboxmanage modifyvm host --name h2
+
+vboxmanage hostonlyif create
+
+vboxmanage modifyvm h1 --nic2 bridged
+vboxmanage modifyvm h1 --bridgeadapter2 vnet-br0-h1
+vboxmanage modifyvm h1 --nic3 hostonly
+vboxmanage modifyvm h1 --hostonlyadapter3 vboxnet0
+
+vboxmanage modifyvm h2 --nic2 bridged
+vboxmanage modifyvm h2 --bridgeadapter2 vnet-br0-h2
+vboxmanage modifyvm h2 --nic3 hostonly
+vboxmanage modifyvm h2 --hostonlyadapter3 vboxnet0
+
+vboxmanage startvm h1 --type headless
+vboxmanage startvm h2 --type headless
---
+++ cloudlab_middle_ovs.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+echo Yong:1|sudo chpasswd
+if [ -z "$1" ]
+then
+	echo "need an argument as a part of ovs dpid\n"
+	echo "such as 01,09,0a,11"
+	exit
+fi
+sudo apt-get update
+sudo apt-get install -y vim openvswitch-switch
+
+sudo ovs-vsctl add-br br0
+sudo ovs-vsctl set bridge br0 other-config:datapath-id=00aa00bb00cc00$1;
+sudo ovs-vsctl set-controller br0 tcp:155.98.39.124:6653
+echo "*********************************"
+sudo ovs-vsctl show
+echo "*********************************"
+sudo ovs-ofctl show br0
+echo "*********************************"
+
